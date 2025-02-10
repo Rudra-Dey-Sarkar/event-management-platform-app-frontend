d38033be94de9d9ff85ff8a244a0b5aecdbd847a
Rudra-Dey-Sarkar <rudradeysarkar5@gmail.com>
2025-02-10 20:58:17 +0530

    fixed

--- components/ClientLayout/ClientLayout.tsx
+++ components/ClientLayout/ClientLayout.tsx
@@ -6,6 +6,7 @@ import { getCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
 import Topbar from '../Topbar/Topbar';
 
+import { usePathname } from 'next/navigation';
 
 type UserDataType = [{
     name: string,
@@ -14,6 +15,7 @@ type UserDataType = [{
 }]
 
 function ClientLayout({ children }: { children: React.ReactNode }) {
+    const pathname = usePathname();
     const router = useRouter();
     const { isPresent, setIsPresent }: any = useContext(GlobalContext);
     const [user, setUser] = useState<UserDataType | any[]>([]);
@@ -40,6 +42,29 @@ function ClientLayout({ children }: { children: React.ReactNode }) {
                 <Topbar />
             }
             <div className='w-full min-h-[100vh]'>
+                {(isPresent === true && (pathname !== "/" && pathname !== "/dashboard")) &&
+                    <div className='p-2'>
+                        <button
+                            className='flex text-[1.2rem] font-bold items-center'
+                            onClick={() => router.back()}>
+                            <svg
+                                width="25px"
+                                height="25px"
+                                viewBox="0 0 1024 1024"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path
+                                    fill="#000000"
+                                    d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+                                />
+                                <path
+                                    fill="#000000"
+                                    d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+                                />
+                            </svg>
+                            <p>Back</p>
+                        </button>
+                    </div>
+                }
                 {children}
             </div>
         </div>
--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,11 +1,10 @@
 "use client"
-import { deleteCookie, getCookie } from 'cookies-next';
+import { getCookie } from 'cookies-next';
 import React, { useContext, useEffect, useState } from 'react'
-import AddEvent from '../AddEvent/AddEvent';
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 import { GlobalContext } from '../../GlobalContext/GlobalContext';
- 
-  
+
+
 type UserDataType = [{
     name: string,
     email: string,
@@ -14,34 +13,34 @@ type UserDataType = [{
 
 function Profile() {
     const router = useRouter();
-        const { isPresent, setIsPresent }: any = useContext(GlobalContext);
-        const [user, setUser] = useState<UserDataType | any[]>([]);
+    const { isPresent, setIsPresent }: any = useContext(GlobalContext);
+    const [user, setUser] = useState<UserDataType | any[]>([]);
 
-    
-        useEffect(() => {
-            const cookies = getCookie("user");
-            if (cookies !== undefined && typeof cookies === "string") {
-                const userCookieData = JSON.parse(cookies);
-                setUser(userCookieData);
-            } else {
-                console.log("cookies not present");
+
+    useEffect(() => {
+        const cookies = getCookie("user");
+        if (cookies !== undefined && typeof cookies === "string") {
+            const userCookieData = JSON.parse(cookies);
+            setUser(userCookieData);
+        } else {
+            console.log("cookies not present");
+        }
+    }, []);
+    return (
+        <div className='w-full h-[100vh] relative flex justify-center items-center'>
+            {(user.length > 0 && user?.[0]?.email === "guest") ?
+                <div>
+                    <p className='text-[2rem] font-bold'>Guest Account</p>
+                </div>
+                :
+                <div>
+                    <p><strong>Name :- </strong>{user?.[0]?.name}</p>
+                    <p><strong>Email :- </strong>{user?.[0]?.email}</p>
+                    <p><strong>Password :- </strong>{user?.[0]?.password}</p>
+                </div>
             }
-        }, []);
-  return (
-    <div className='w-full h-[100vh] flex justify-center items-center'>
-        {(user.length > 0 && user?.[0]?.email === "guest") ? 
-         <div>
-              <p className='text-[2rem] font-bold'>Guest Account</p>
-         </div>
-         :
-         <div>
-             <p><strong>Name :- </strong>{user?.[0]?.name}</p>
-             <p><strong>Email :- </strong>{user?.[0]?.email}</p>
-             <p><strong>Password :- </strong>{user?.[0]?.password}</p>
-         </div>
-          }
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Profile
